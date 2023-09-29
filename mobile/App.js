4ae17a90e51ebecc2178204a60c08f8618aa3cb6
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2023-09-28 21:35:10 -0300

    chore: updated app and map view

--- mobile/App.js
+++ mobile/App.js
@@ -1,32 +1,66 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
 
-import { WebView } from "react-native-webview";
-import { StyleSheet, Text, View } from "react-native";
+import Device from "expo-device";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
+
+import { Platform, Text, View, StyleSheet } from "react-native";
 
 const App = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
-    <>
-      <WebView
-        style={styles.container}
-        source={{ uri: "https://thiagopetry.github.io/whereiam/" }}
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>{text}</Text> */}
+
+      <MapView
+        style={styles.map}
+        mapType={"hybrid"}
+        showsBuildings={true}
+        showsUserLocation={true}
+        provider={PROVIDER_GOOGLE}
+        showsMyLocationButton={true}
       />
-      <StatusBar style="auto" />
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
-    backgroundColor: "#333",
     alignItems: "center",
     justifyContent: "center",
   },
-  webview: {
+  text: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+  map: {
     width: "100%",
     height: "100%",
-    backgroundColor: "red",
   },
 });
 
